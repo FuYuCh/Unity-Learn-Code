2d932e531b5e4f711e9b8c9aca4fcd6909c549d2
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-05 15:38:29 +0800

    重新設定位置

--- Unity-Learn-Code/Assets/練習2/GameManager.cs
+++ Unity-Learn-Code/Assets/練習2/GameManager.cs
@@ -4,15 +4,12 @@ public class GameManager : MonoBehaviour
 {
     public Soldier SoldierA = new Soldier();
 
-    private void start()
+    private void Start()
     {
         SoldierA.lv = 30;
         Debug.Log("士兵經驗值 - 取得:" + SoldierA.GetExp());
-
+        SoldierA.exp = 50;
         SoldierA.SetExp(100);
         Debug.Log("士兵經驗值 - 設定" + SoldierA.exp);
     }
-
-
-
 }
