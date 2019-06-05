5d083d45437f16d1dd3a2493f7cc91715012331a
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-05 13:14:20 +0800

    新增狗狗年齡，吃東西的方法

--- Unity-Learn-Code/Assets/Scripts/Dog.cs
+++ Unity-Learn-Code/Assets/Scripts/Dog.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;  
+﻿using UnityEngine;   //引用 API
+using System;
 
 
-public class Dog
+public class Dog    //建立類別
 {
     #region 欄位     //可以包括住程式碼 
     public string name;
@@ -13,9 +14,40 @@ public class Dog
     #endregion
 
     #region 方法
+    /// <summary>
+    /// 狗狗叫的方法
+    /// </summary>
     public void Bark()
     {
         Debug.Log(name + ":汪汪");
     }
+
+    /// <summary>
+    /// 轉換狗狗年齡的方法
+    /// </summary>
+    /// <returns>狗狗年齡</returns>
+    public int ConvertAge()
+    {
+        return age * 7;
+    }
+    /// <summary>
+    /// 狗狗吃東西的方法
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="speed"></param>
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + ":" + "food" + "，速度:" + speed);
+    }
+    /// <summary>
+    /// 狗狗吃東西的方法
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="count"></param>
+    public void Eat(string food, int count)
+    {
+        Debug.Log(name + ":" + food + "，份量" + count);
+    }
     #endregion
+
 }
