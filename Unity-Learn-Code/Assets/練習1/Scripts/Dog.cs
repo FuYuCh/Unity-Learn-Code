b454c60055c62c1f034fea63cb749b5a7aa1bb12
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-05 15:00:54 +0800

    新增士兵，等級、經驗值

--- Unity-Learn-Code/Assets/練習1/Scripts/Dog.cs
+++ Unity-Learn-Code/Assets/練習1/Scripts/Dog.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;   //引用 API
 
-[System.Serializable] // 它把會把裡面的內容序列化，
-                      // 公開的欄位會顯示在屬性面板上
+[System.Serializable] // 序列化的類別會連同欄位顯示在屬性面板上
 public class Dog    //建立類別
 {
     #region 欄位     //可以包括住程式碼 
